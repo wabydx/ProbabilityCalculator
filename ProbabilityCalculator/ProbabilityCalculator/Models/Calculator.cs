44c46887fccf29097ddeffb6b6d229fecfd3a0f9
unknown <dane.waby@CP8133DNQA3CNF8.dir.svc.accenture.com>
2015-09-21 19:35:31 +0100

    first draft - no comments, no style etc

---
+++ ProbabilityCalculator/ProbabilityCalculator/Models/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProbabilityCalculator.Models
+{
+    public class Calculator
+    {
+        public decimal ProbabilityOne { get; set; }
+
+        public decimal ProbabilityTwo { get; set; }
+
+        public Function SelectedFunction { get; set; }
+
+        public decimal Result { get; set; }
+
+        public enum Function
+        {
+            CombinedWith,
+            Either
+        }
+    }
+}
---
+++ ProbabilityCalculator/ProbabilityCalculator/Scripts/site.js
@@ -0,0 +1,62 @@
+﻿function Calculate() {
+    if (ValidateValues()) {
+        var probabilityOne = parseFloat($('#ProbabilityOne').val());
+        var probabilityTwo = parseFloat($('#ProbabilityTwo').val());
+        var selectedOption = $('#SelectedFunction').val();
+
+        var result = GetResult(probabilityOne, probabilityTwo, selectedOption);
+        $('#resultLabel').html("Probability result is: " + result);
+
+        //SaveResult(probabilityOne, probabilityOne, selectedOption, result);
+    }
+}
+
+function ValidateValues() {
+    var value1 = false;
+    var value2 = false;
+    var errorMessage = 'Value must be between 0.0 and 1.0';
+    var probabilityOne = parseFloat($('#ProbabilityOne').val());
+    var probabilityTwo = parseFloat($('#ProbabilityTwo').val());
+
+    if (isNaN(probabilityOne) || (probabilityOne < 0.0 || probabilityOne > 1.0)) {
+        $('#probabilityOneError').html(errorMessage);
+        $('#resultLabel').html('');
+    } else {
+        $('#probabilityOneError').html('');
+        value1 = true;
+    }
+
+    if (isNaN(probabilityTwo) || (probabilityTwo < 0.0 || probabilityTwo > 1.0)) {
+        $('#probabilityTwoError').html(errorMessage);
+        $('#resultLabel').html('');
+    } else {
+        $('#probabilityTwoError').html('');
+        value2 = true;
+    }
+
+    return value1 && value2;
+}
+
+function GetResult(probabilityOne, probabilityTwo, selectedOption) {
+    var result = "Error calculating probability";
+
+    if (selectedOption === '0') {
+        result = probabilityOne * probabilityTwo;
+    }
+    else if (selectedOption === '1') {
+        result = probabilityOne + probabilityTwo - (probabilityOne * probabilityTwo);
+    }
+
+    return result;
+}
+
+function SaveResult(probabilityOne, probabilityTwo, selectedFunction, result) {
+    $.ajax({
+        url: '/Home/SaveResult',
+        contentType: 'application/json',
+        type: 'POST',
+        data: JSON.stringify({ probabilityOne: probabilityOne, probabilityTwo: probabilityTwo, selectedFunction: selectedFunction, result: result }),  
+        });
+}
+
+
